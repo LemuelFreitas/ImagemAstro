96f06bb516da1c9eb2203f97d5c5b3a1fedb3d55
LemuelFreitas <lemuel.zuin.freitas@gmail.com>
2024-03-26 13:49:34 -0300

    ImagemAstro

---
+++ AppStyle.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+    container: {
+      flex: 1,
+      backgroundColor:'#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+     
+      
+    },
+    imagens:{
+    height: 680,
+    width:400,
+    
+    },
+    texto: {
+      color:'#fff',
+      textAlign:'center',
+    }
+  });
+  
